8c42ed0adb81f9ed39d3bfe782d154e7c7c6ef95
Ashwinmurugan <120318876+Ashwinmurugan@users.noreply.github.com>
2024-04-11 18:10:08 +0530

    Update Banner.jsx

--- IMBD_Movies/src/components/Banner.jsx
+++ IMBD_Movies/src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Banner({ movie }) {
   return (
-    <div className='h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end ' style={{backgroundImage : `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}}>
+    <div className='h-[20vh] md:h-[75vh] bg-cover bg-center flex items-end ' style={{backgroundImage : `url(API)`}}>
       <div className='text-white font-bold text-xl text-center w-full bg-gray-600/60  my-3'>{movie.original_title}</div>
     </div>
   );
